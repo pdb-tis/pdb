52bbd994c7e12c456ca807a798e04b1005dfbccf
Susanoo <george.melgaco@gmail.com>
2017-04-27 01:28:29 -0300

    SQL scripts translated from MySQL syntax to T-SQL

--- db-docs/pdb-tis-db-model.sql
+++ db-docs/pdb-tis-db-model.sql
@@ -5,8 +5,7 @@ CREATE TABLE `address`
   `line_2` varchar(50),
   `number` smallint unsigned,
   `complement` varchar(20),
-  `city` varchar(20) NOT NULL,
-  `province` varchar(20) NOT NULL,
+  `city_id` integer unsigned NOT NULL,
   CONSTRAINT pk_address PRIMARY KEY (`id`)
 );
 
@@ -41,11 +40,63 @@ CREATE TABLE `band_member`
   CONSTRAINT pk_band_member PRIMARY KEY (`id`)
 );
 
+CREATE TABLE `city`
+(
+  `id` integer unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  `province_id` tinyint unsigned NOT NULL,
+  CONSTRAINT pk_city PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `customer`
+(
+  `id` integer unsigned NOT NULL AUTO_INCREMENT,
+  `type_id` tinyint unsigned NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `phone` varchar(20),
+  CONSTRAINT pk_customer PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `customer_address`
+(
+  `customer_id` integer unsigned NOT NULL,
+  `address_id` integer unsigned NOT NULL
+);
+
+CREATE TABLE `customer_musical_genre`
+(
+  `customer_id` integer unsigned NOT NULL,
+  `musical_genre_id` integer unsigned NOT NULL
+);
+
+CREATE TABLE `customer_picture`
+(
+  `customer_id` integer unsigned NOT NULL,
+  `picture_id` bigint unsigned NOT NULL
+);
+
+CREATE TABLE `customer_social_network`
+(
+  `id` integer unsigned NOT NULL AUTO_INCREMENT,
+  `customer_id` integer unsigned NOT NULL,
+  `social_network_type_id` tinyint unsigned NOT NULL,
+  `external_link` varchar(100) NOT NULL,
+  CONSTRAINT pk_customer_social_network PRIMARY KEY (`id`)
+);
+
 CREATE TABLE `en_band_type`
 (
   `id` tinyint unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(20) NOT NULL,
   CONSTRAINT pk_en_band_type PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `en_customer_type`
+(
+  `id` tinyint unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(20) NOT NULL,
+  CONSTRAINT pk_en_customer_type PRIMARY KEY (`id`)
 );
 
 CREATE TABLE `en_musical_genre`
@@ -69,13 +120,6 @@ CREATE TABLE `en_social_network_type`
   `external_link` varchar(100) NOT NULL,
   `picture_id` bigint unsigned NOT NULL,
   CONSTRAINT pk_en_social_network PRIMARY KEY (`id`)
-);
-
-CREATE TABLE `en_user_type`
-(
-  `id` tinyint unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(20) NOT NULL,
-  CONSTRAINT pk_en_user_type PRIMARY KEY (`id`)
 );
 
 CREATE TABLE `event`
@@ -129,115 +173,92 @@ CREATE TABLE `picture`
   CONSTRAINT pk_picture PRIMARY KEY (`id`)
 );
 
-CREATE TABLE `user`
+CREATE TABLE `province`
 (
-  `id` integer unsigned NOT NULL AUTO_INCREMENT,
-  `type_id` tinyint unsigned NOT NULL,
-  `name` varchar(50) NOT NULL,
-  `email` varchar(50) NOT NULL,
-  `phone` varchar(20),
-  CONSTRAINT pk_user PRIMARY KEY (`id`)
+  `id` tinyint unsigned NOT NULL,
+  `name` varchar(20) NOT NULL,
+  `initials` varchar(2) NOT NULL,
+  CONSTRAINT pk_province PRIMARY KEY (`id`)
 );
 
-CREATE TABLE `user_address`
-(
-  `user_id` integer unsigned NOT NULL,
-  `address_id` integer unsigned NOT NULL
-);
-
-CREATE TABLE `user_musical_genre`
-(
-  `user_id` integer unsigned NOT NULL,
-  `musical_genre_id` integer unsigned NOT NULL
-);
-
-CREATE TABLE `user_picture`
-(
-  `user_id` integer unsigned NOT NULL,
-  `picture_id` bigint unsigned NOT NULL
-);
-
-CREATE TABLE `user_social_network`
-(
-  `id` integer unsigned NOT NULL AUTO_INCREMENT,
-  `user_id` integer unsigned NOT NULL,
-  `social_network_type_id` tinyint unsigned NOT NULL,
-  `external_link` varchar(100) NOT NULL,
-  CONSTRAINT pk_user_social_network PRIMARY KEY (`id`)
-);
+ALTER TABLE `address` ADD CONSTRAINT `fk_address_city`
+  FOREIGN KEY (`city_id`) REFERENCES `city` (`id`) ON UPDATE NO ACTION;
 
 ALTER TABLE `album` ADD CONSTRAINT `fk_album_band`
-  FOREIGN KEY (`band_id`) REFERENCES `band` (`id`) ON UPDATE NO ACTION;
+  FOREIGN KEY (`band_id`) REFERENCES `band` (`id`);
 
 ALTER TABLE `album` ADD CONSTRAINT `fk_album_picture`
-  FOREIGN KEY (`picture_id`) REFERENCES `picture` (`id`) ON UPDATE CASCADE;
+  FOREIGN KEY (`picture_id`) REFERENCES `picture` (`id`);
 
 ALTER TABLE `band` ADD CONSTRAINT `fk_band_band_type`
-  FOREIGN KEY (`type_id`) REFERENCES `en_band_type` (`id`) ON UPDATE NO ACTION;
+  FOREIGN KEY (`type_id`) REFERENCES `en_band_type` (`id`);
 
 ALTER TABLE `band` ADD CONSTRAINT `fk_band_birth_address`
-  FOREIGN KEY (`birth_address_id`) REFERENCES `address` (`id`) ON UPDATE CASCADE;
+  FOREIGN KEY (`birth_address_id`) REFERENCES `address` (`id`);
 
 ALTER TABLE `band` ADD CONSTRAINT `fk_band_user`
-  FOREIGN KEY (`id`) REFERENCES `user` (`id`) ON UPDATE CASCADE;
+  FOREIGN KEY (`id`) REFERENCES `customer` (`id`);
 
 ALTER TABLE `band_member` ADD CONSTRAINT `fk_band_member_band`
-  FOREIGN KEY (`band_id`) REFERENCES `band` (`id`) ON UPDATE NO ACTION;
+  FOREIGN KEY (`band_id`) REFERENCES `band` (`id`);
+
+ALTER TABLE `city` ADD CONSTRAINT `fk_city_province`
+  FOREIGN KEY (`province_id`) REFERENCES `province` (`id`) ON UPDATE NO ACTION;
+
+ALTER TABLE `customer` ADD CONSTRAINT `fk_customer_en_customer_type`
+  FOREIGN KEY (`type_id`) REFERENCES `en_customer_type` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+ALTER TABLE `customer_address` ADD CONSTRAINT `fk_customer_address_address`
+  FOREIGN KEY (`address_id`) REFERENCES `address` (`id`);
+
+ALTER TABLE `customer_address` ADD CONSTRAINT `fk_customer_address_user`
+  FOREIGN KEY (`customer_id`) REFERENCES `customer` (`id`);
+
+ALTER TABLE `customer_musical_genre` ADD CONSTRAINT `fk_customer_musical_genre_musical_genre`
+  FOREIGN KEY (`musical_genre_id`) REFERENCES `en_musical_genre` (`id`);
+
+ALTER TABLE `customer_musical_genre` ADD CONSTRAINT `fk_customer_musical_genre_user`
+  FOREIGN KEY (`customer_id`) REFERENCES `customer` (`id`);
+
+ALTER TABLE `customer_picture` ADD CONSTRAINT `fk_customer_picture_customer`
+  FOREIGN KEY (`customer_id`) REFERENCES `customer` (`id`);
+
+ALTER TABLE `customer_picture` ADD CONSTRAINT `fk_customer_picture_picture`
+  FOREIGN KEY (`picture_id`) REFERENCES `picture` (`id`);
+
+ALTER TABLE `customer_social_network` ADD CONSTRAINT `fk_customer_social_network_customer`
+  FOREIGN KEY (`customer_id`) REFERENCES `customer` (`id`);
+
+ALTER TABLE `customer_social_network` ADD CONSTRAINT `fk_customer_social_network_en_social_network_type`
+  FOREIGN KEY (`social_network_type_id`) REFERENCES `en_social_network_type` (`id`);
 
 ALTER TABLE `en_musical_genre_sub_genre` ADD CONSTRAINT `fk_en_musical_genre`
-  FOREIGN KEY (`genre_id`) REFERENCES `en_musical_genre` (`id`) ON UPDATE NO ACTION;
+  FOREIGN KEY (`genre_id`) REFERENCES `en_musical_genre` (`id`);
 
 ALTER TABLE `en_musical_genre_sub_genre` ADD CONSTRAINT `fk_en_musical_genre_sub_genre`
-  FOREIGN KEY (`sub_genre_id`) REFERENCES `en_musical_genre` (`id`) ON UPDATE NO ACTION;
+  FOREIGN KEY (`sub_genre_id`) REFERENCES `en_musical_genre` (`id`);
 
 ALTER TABLE `event_band` ADD CONSTRAINT `fk_event_band_band`
-  FOREIGN KEY (`band_id`) REFERENCES `band` (`id`) ON UPDATE NO ACTION;
+  FOREIGN KEY (`band_id`) REFERENCES `band` (`id`);
 
 ALTER TABLE `event_band` ADD CONSTRAINT `fk_event_band_event`
-  FOREIGN KEY (`event_id`) REFERENCES `event` (`id`) ON UPDATE NO ACTION;
+  FOREIGN KEY (`event_id`) REFERENCES `event` (`id`);
 
 ALTER TABLE `event_picture` ADD CONSTRAINT `fk_event_picture_event`
-  FOREIGN KEY (`event_id`) REFERENCES `event` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+  FOREIGN KEY (`event_id`) REFERENCES `event` (`id`);
 
 ALTER TABLE `event_picture` ADD CONSTRAINT `fk_event_picture_picture`
-  FOREIGN KEY (`picture_id`) REFERENCES `picture` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION;
+  FOREIGN KEY (`picture_id`) REFERENCES `picture` (`id`);
 
 ALTER TABLE `event_social_network` ADD CONSTRAINT `fk_event_social_network_en_social_network_type`
-  FOREIGN KEY (`social_network_type_id`) REFERENCES `en_social_network_type` (`id`) ON UPDATE NO ACTION;
+  FOREIGN KEY (`social_network_type_id`) REFERENCES `en_social_network_type` (`id`);
 
 ALTER TABLE `event_social_network` ADD CONSTRAINT `fk_event_social_network_event`
-  FOREIGN KEY (`event_id`) REFERENCES `event` (`id`) ON UPDATE NO ACTION;
+  FOREIGN KEY (`event_id`) REFERENCES `event` (`id`);
 
 ALTER TABLE `fan` ADD CONSTRAINT `fk_fan_birth_address`
-  FOREIGN KEY (`birth_address_id`) REFERENCES `address` (`id`) ON UPDATE CASCADE;
+  FOREIGN KEY (`birth_address_id`) REFERENCES `address` (`id`);
 
 ALTER TABLE `fan` ADD CONSTRAINT `fk_fan_user`
-  FOREIGN KEY (`id`) REFERENCES `user` (`id`) ON UPDATE CASCADE;
-
-ALTER TABLE `user` ADD CONSTRAINT `fk_user_en_user_type`
-  FOREIGN KEY (`type_id`) REFERENCES `en_user_type` (`id`) ON UPDATE NO ACTION;
-
-ALTER TABLE `user_address` ADD CONSTRAINT `fk_user_address_address`
-  FOREIGN KEY (`address_id`) REFERENCES `address` (`id`) ON UPDATE CASCADE;
-
-ALTER TABLE `user_address` ADD CONSTRAINT `fk_user_address_user`
-  FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON UPDATE CASCADE;
-
-ALTER TABLE `user_musical_genre` ADD CONSTRAINT `fk_user_musical_genre_musical_genre`
-  FOREIGN KEY (`musical_genre_id`) REFERENCES `en_musical_genre` (`id`) ON UPDATE NO ACTION;
-
-ALTER TABLE `user_musical_genre` ADD CONSTRAINT `fk_user_musical_genre_user`
-  FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON UPDATE NO ACTION;
-
-ALTER TABLE `user_picture` ADD CONSTRAINT `fk_user_picture_picture`
-  FOREIGN KEY (`picture_id`) REFERENCES `picture` (`id`) ON UPDATE CASCADE;
-
-ALTER TABLE `user_picture` ADD CONSTRAINT `fk_user_picture_user`
-  FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON UPDATE NO ACTION;
-
-ALTER TABLE `user_social_network` ADD CONSTRAINT `fk_user_social_network_en_social_network_type`
-  FOREIGN KEY (`social_network_type_id`) REFERENCES `en_social_network_type` (`id`) ON UPDATE NO ACTION;
-
-ALTER TABLE `user_social_network` ADD CONSTRAINT `fk_user_social_network_user`
-  FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON UPDATE NO ACTION;
+  FOREIGN KEY (`id`) REFERENCES `customer` (`id`);
 
