33a4e37f06bee2238798a8ce99860a552f42d84e
Zuquim <mtulio.z@gmail.com>
2017-05-04 19:03:28 -0300

    JS_datepicker implementation in progress (01)

--- js/datepicker.js
+++ js/datepicker.js
@@ -1,8 +1,15 @@
-function datePicker() {
-    $( "#datepicker" ).datepicker({
+function () {
+    ( "#datepicker" ).datepicker({
     showOn: "button",
     buttonImage: "../images/calendar-icon.png",
     buttonImageOnly: true,
     buttonText: "Selecione uma data"
     });
 }
+/*
+function() {
+  $("#datepicker-my").datepicker();
+  $('#btn').click(function() {
+    $("#datepicker-my").focus();
+  });
+}*/
