db81acbeffc3ccf5c1d45ae81f289f4ff04d7cd9
Zuquim <mtulio.z@gmail.com>
2017-05-04 20:35:20 -0300

    jQuery datepicker implementation (02)

--- js/datepicker.js
+++ js/datepicker.js
@@ -1,15 +1,8 @@
-function () {
-    ( "#datepicker" ).datepicker({
-    showOn: "button",
-    buttonImage: "../images/calendar-icon.png",
-    buttonImageOnly: true,
-    buttonText: "Selecione uma data"
+$(function() {
+    $( "#datepicker" ).datepicker({
+        showOn: "button",
+        buttonImage: "../images/calendar-icon.png",
+        buttonImageOnly: true,
+        buttonText: "Selecione uma data"
     });
-}
-/*
-function() {
-  $("#datepicker-my").datepicker();
-  $('#btn').click(function() {
-    $("#datepicker-my").focus();
-  });
-}*/
+  } );
