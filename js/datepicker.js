26b03db054347168c394e94161f34c44b3ecca8c
Zuquim <mtulio.z@gmail.com>
2017-05-05 02:08:18 -0300

    Homepage carousel slideshow implemented

--- js/datepicker.js
+++ js/datepicker.js
@@ -1,8 +1,39 @@
-$(function() {
+$(function () {
     $( "#datepicker" ).datepicker({
         showOn: "button",
         buttonImage: "../images/calendar-icon.png",
         buttonImageOnly: true,
         buttonText: "Selecione uma data"
     });
+  });/*
+  $( function() {
+    var dateFormat = "mm/dd/yy",
+      from = $( "#from" )
+        .datepicker({
+          defaultDate: "+1w",
+          changeMonth: true,
+          numberOfMonths: 3
+        })
+        .on( "change", function() {
+          to.datepicker( "option", "minDate", getDate( this ) );
+        }),
+      to = $( "#to" ).datepicker({
+        defaultDate: "+1w",
+        changeMonth: true,
+        numberOfMonths: 3
+      })
+      .on( "change", function() {
+        from.datepicker( "option", "maxDate", getDate( this ) );
+      });
+
+    function getDate( element ) {
+      var date;
+      try {
+        date = $.datepicker.parseDate( dateFormat, element.value );
+      } catch( error ) {
+        date = null;
+      }
+
+      return date;
+    }
   } );
